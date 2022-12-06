007bc77fe1fa396cf224d744402999793bbb1dbb
Ted Slocum <sted9000@gmail.com>
2022-12-06 10:39:50 -0500

    simplier for loop: enumerate -> range

--- part_2/06.py
+++ part_2/06.py
@@ -6,7 +6,7 @@ def no_duplicates(my_list):
 packet_length = 14
 with open('../inputs/06.txt') as f:
     line = f.read().splitlines()[0]
-    for i, char in enumerate(line):
+    for i in range(len(line)):
         packet = line[i:i + packet_length]
         if no_duplicates(packet):
             print(i + packet_length)
